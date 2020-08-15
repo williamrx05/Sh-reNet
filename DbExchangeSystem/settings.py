d44c10a5f2bb5b0bb456eb53b0225296f85cff57
williamrx05 <66583944+williamrx05@users.noreply.github.com>
2020-08-15 19:47:20 -0400

    updated database for hosting

--- DbExchangeSystem/settings.py
+++ DbExchangeSystem/settings.py
@@ -80,13 +80,24 @@ WSGI_APPLICATION = 'DbExchangeSystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'myproject',
+            'USER': 'william',
+            'PASSWORD': '1234123four',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
