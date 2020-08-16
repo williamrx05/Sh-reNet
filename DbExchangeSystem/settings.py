cb268b37cbb06668720e629533a389551a6a7ad2
williamrx05 <66583944+williamrx05@users.noreply.github.com>
2020-08-15 20:31:44 -0400

    updated databases

--- DbExchangeSystem/settings.py
+++ DbExchangeSystem/settings.py
@@ -91,7 +91,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'ShareNet',
+            'NAME': 'sharenet',
             'USER': 'william',
             'PASSWORD': '1234123four',
             'HOST': 'localhost',
